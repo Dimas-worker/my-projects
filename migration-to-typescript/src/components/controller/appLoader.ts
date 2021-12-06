04ed59e7671d6f98ef7416f09f921a8fb2ccdbde
Dmitry_Petralai <pdo.hardwork@yandex.ru>
2021-12-06 22:03:51 +0300

    refactor: add everyday types according to the task

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', { //https://nodenews.herokuapp.com/
+        super('https://nodenews.herokuapp.com/', { //https://newsapi.org/v2/ change on https://nodenews.herokuapp.com/
             apiKey: 'd26c10b1b92e4db2be9de4f444090d6e', // получите свой ключ https://newsapi.org/
         });
     }
--- migration-to-typescript/src/components/controller/loader.ts
+++ migration-to-typescript/src/components/controller/loader.ts
@@ -1,19 +1,28 @@
+export type DataUrlOptions = {
+    [index: string]: string;
+};
+
+type defaultOption = {
+    sources?: string;
+};
 class Loader {
-    constructor(baseLink, options) {
+    protected baseLink: string;
+    protected options: { apiKey: string };
+    constructor(baseLink: string, options: { apiKey: string }) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
-        callback = () => {
+        { endpoint, options = {} }: { endpoint: string; options?: defaultOption },
+        callback: <T>(data: T) => void = () => {
             console.error('No callback for GET response');
         }
-    ) {
-        this.load('GET', endpoint, callback, options);
+    ): void {
+        this.load("GET", endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    errorHandler(res: Response): Response {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,18 +32,18 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
-        const urlOptions = { ...this.options, ...options };
+    makeUrl(options: defaultOption, endpoint: string): string {
+        const urlOptions: DataUrlOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
-        Object.keys(urlOptions).forEach((key) => {
+        Object.keys(urlOptions).forEach((key: string) => {
             url += `${key}=${urlOptions[key]}&`;
         });
 
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load(method:string, endpoint: string, callback: (data:object) => void, options = {}): void {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
