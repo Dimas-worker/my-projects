6189ae68b871c2bcbe0c211045ccdd9b195dddb2
Dmitry_Petralai <pdo.hardwork@yandex.ru>
2021-12-07 00:54:43 +0300

    feat: improve the visual part

--- migration-to-typescript/src/components/app/app.ts
+++ migration-to-typescript/src/components/app/app.ts
@@ -4,19 +4,18 @@ import AppView from '../view/appView';
 class App {
     private controller: AppController;
     private view: AppView;
-  
+
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
-  
+
     start(): void {
         document
             .querySelector('.sources')
             ?.addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
         this.controller.getSources((data) => this.view.drawSources(data));
     }
-  }
-  
-  export default App;
-  
+}
+
+export default App;
--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://nodenews.herokuapp.com/', { //https://newsapi.org/v2/ change on https://nodenews.herokuapp.com/
-            apiKey: 'd26c10b1b92e4db2be9de4f444090d6e', // получите свой ключ https://newsapi.org/
+        super('https://nodenews.herokuapp.com/', {
+            apiKey: 'd26c10b1b92e4db2be9de4f444090d6e',
         });
     }
 }
